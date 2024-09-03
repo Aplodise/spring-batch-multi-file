915440aeec42fd062c0b9bf047eb546839bf8168
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-03 15:12:42 +0200

    Scheduling enabled, properties modified

--- src/main/java/com/roman/multi_file_processing/MultiFileProcessingApplication.java
+++ src/main/java/com/roman/multi_file_processing/MultiFileProcessingApplication.java
@@ -2,8 +2,10 @@ package com.roman.multi_file_processing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MultiFileProcessingApplication {
 
 	public static void main(String[] args) {
