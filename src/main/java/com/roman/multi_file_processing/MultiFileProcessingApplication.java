1b204ca5f099c613d551a5de9d4a7a1aa3d37dbf
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-03 13:13:12 +0200

    Initial Commit

---
+++ src/main/java/com/roman/multi_file_processing/MultiFileProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.roman.multi_file_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiFileProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiFileProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roman/multi_file_processing/MultiFileProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman.multi_file_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiFileProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
