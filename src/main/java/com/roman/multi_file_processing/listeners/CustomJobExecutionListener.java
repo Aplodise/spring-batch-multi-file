dcbc3a4deddc02ba10b41fc2cab32a156ce8df31
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-03 15:15:48 +0200

    Job Execution Listener

---
+++ src/main/java/com/roman/multi_file_processing/listeners/CustomJobExecutionListener.java
@@ -0,0 +1,23 @@
+package com.roman.multi_file_processing.listeners;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CustomJobExecutionListener implements JobExecutionListener {
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        jobExecution.getStepExecutions()
+                .stream()
+                .findFirst()
+                .ifPresent(stepExecution -> {
+                    long writeCount = stepExecution.getWriteCount();
+                    log.info(String.format("the job has written %s lines", writeCount));
+                });
+
+    }
+}
